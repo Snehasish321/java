b48f632d412da9d4d64d90dd2dfe6acc8a38d2df
Snehasish321 <snehasishmondal01@gmail.com>
2025-03-23 10:45:05 +0530

    variableDone

---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    
+
+    public static void main (String[] args) {
+        int age = 20;
+        String name = "Snehasish";
+
+        System.out.println("My name is " + name + " and my age is " + age);
+    }
+}
